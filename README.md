a81565fcc1c831719b9f90635b418360e8cdc3fa
xorvet <cypherx702@gmail.com>
2023-03-09 16:11:20 +0530

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+#### init.nvim
+```
+Packeges used :
+  - packer.nvim    (Plugin Manager)
+  - lsp-zero.nvim  (LSP Support , Auto Completion , Snippets)
+  - lualine.nvim   (Statusline)
+```
---
+++ lua/main/init.lua
@@ -0,0 +1,50 @@
+require("main.packer")
+
+--Theme
+require('lualine').setup({
+    options = { 
+        theme = 'ayu_dark',
+        icons_enabled = false,
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+    }
+})
+
+--LSP
+local lsp = require('lsp-zero').preset({
+    name = 'minimal',
+    set_lsp_keymaps = true,
+    manage_nvim_cmp = true,
+    suggest_lsp_servers = false,
+})
+
+local cmp = require('cmp')
+
+lsp.setup_nvim_cmp({
+    preselect = 'none',
+    completion = {
+        completeopt = 'menu,menuone,noinsert,noselect'
+    },
+    mapping = lsp.defaults.cmp_mappings({
+        ['<Tab>'] = cmp.mapping.select_next_item()
+    })
+})
+
+lsp.setup()
+
+--set
+vim.opt.nu = true
+vim.opt.tabstop = 3
+vim.opt.guicursor = ""
+vim.opt.smartindent = true
+vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
+vim.opt.showmode = false
+vim.opt.clipboard = 'unnamedplus'
+
+--remap
+vim.keymap.set('n','pv',vim.cmd.Ex)
+vim.keymap.set('v','<C-c>','"+y')
+vim.keymap.set('v','<C-x>','"+x')
+vim.keymap.set('v','<C-l>','"+yy')
