acf0b8672000271bbf2daabe7195cd9e8862c4cf
xorvet <cypherx702@gmail.com>
2023-03-11 20:42:19 +0530

    changes

--- lua/main/init.lua
+++ lua/main/init.lua
@@ -2,32 +2,32 @@ require("main.packer")
 
 --Theme
 require('lualine').setup({
-    options = { 
-        theme = 'ayu_dark',
-        icons_enabled = false,
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-    }
+   options = {
+      theme = 'ayu_dark',
+      icons_enabled = true,
+      component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
+   }
 })
 
 --LSP
 local lsp = require('lsp-zero').preset({
-    name = 'minimal',
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
+   name = 'minimal',
+   set_lsp_keymaps = true,
+   manage_nvim_cmp = true,
+   suggest_lsp_servers = false,
 })
 
 local cmp = require('cmp')
 
 lsp.setup_nvim_cmp({
-    preselect = 'none',
-    completion = {
-        completeopt = 'menu,menuone,noinsert,noselect'
-    },
-    mapping = lsp.defaults.cmp_mappings({
-        ['<Tab>'] = cmp.mapping.select_next_item()
-    })
+   preselect = 'none',
+   completion = {
+      completeopt = 'menu,menuone,noinsert,noselect'
+   },
+   mapping = lsp.defaults.cmp_mappings({
+      ['<Tab>'] = cmp.mapping.select_next_item()
+   })
 })
 
 lsp.setup()
@@ -48,3 +48,4 @@ vim.keymap.set('n','pv',vim.cmd.Ex)
 vim.keymap.set('v','<C-c>','"+y')
 vim.keymap.set('v','<C-x>','"+x')
 vim.keymap.set('v','<C-l>','"+yy')
+vim.api.nvim_set_keymap('v', 'gg=G', '<ESC><cmd>lua vim.lsp.buf.format()<CR>', {noremap = true})
