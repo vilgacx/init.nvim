e71de5c974626b9fece4c4881463013664dff21e
xorvet <cypherx702@gmail.com>
2023-03-21 21:20:38 +0530

    remaps

--- lua/main/init.lua
+++ lua/main/init.lua
@@ -2,43 +2,45 @@ require("main.packer")
 
 --Theme
 require('lualine').setup({
-   options = {
-      theme = 'ayu_dark',
-      icons_enabled = true,
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
-   }
+  options = {
+    theme = 'ayu_dark',
+    icons_enabled = true,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+  }
 })
 
 --LSP
 local lsp = require('lsp-zero').preset({
-   name = 'minimal',
-   set_lsp_keymaps = true,
-   manage_nvim_cmp = true,
-   suggest_lsp_servers = false,
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
 })
 
 local cmp = require('cmp')
 
 lsp.setup_nvim_cmp({
-   preselect = 'none',
-   completion = {
-      completeopt = 'menu,menuone,noinsert,noselect'
-   },
-   mapping = lsp.defaults.cmp_mappings({
-      ['<Tab>'] = cmp.mapping.select_next_item()
-   })
+  preselect = 'none',
+  completion = {
+    completeopt = 'menu,menuone,noinsert,noselect'
+  },
+  mapping = lsp.defaults.cmp_mappings({
+    ['<Tab>'] = cmp.mapping.select_next_item()
+  })
 })
 
 lsp.setup()
 
+require("flutter-tools").setup { settings = { enableSnippets = true } }
+
 --set
 vim.opt.nu = true
-vim.opt.tabstop = 3
+vim.opt.tabstop = 2
 vim.opt.guicursor = ""
 vim.opt.smartindent = true
-vim.opt.softtabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.showmode = false
 vim.opt.clipboard = 'unnamedplus'
@@ -47,5 +49,10 @@ vim.opt.clipboard = 'unnamedplus'
 vim.keymap.set('n','pv',vim.cmd.Ex)
 vim.keymap.set('v','<C-c>','"+y')
 vim.keymap.set('v','<C-x>','"+x')
-vim.keymap.set('v','<C-l>','"+yy')
 vim.api.nvim_set_keymap('v', 'gg=G', '<ESC><cmd>lua vim.lsp.buf.format()<CR>', {noremap = true})
+vim.keymap.set('n','<C-b><Up>','<C-w><Up>')
+vim.keymap.set('n','<C-b><Down>','<C-w><Down>')
+vim.keymap.set('n','<C-b><Left>','<C-w><Left>')
+vim.keymap.set('n','<C-b><Right>','<C-w><Right>')
+vim.keymap.set('n','<C-b>h',vim.cmd.sp)
+vim.keymap.set('n','<C-b>v',vim.cmd.vsp)
--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -1,27 +1,33 @@
 vim.cmd.packadd('packer.nvim')
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {'nvim-lualine/lualine.nvim',requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+  use 'wbthomason/packer.nvim'
+  use {'nvim-lualine/lualine.nvim',requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lua'},
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
     }
+  }
+  use {
+    'akinsho/flutter-tools.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+    },
+  }
 end)
