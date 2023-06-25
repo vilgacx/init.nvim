a6de970dabf3cad8fa61ecff5b2962913ae5fa04
xorvet <cypherx702@gmail.com>
2023-06-25 19:07:46 +0530

    cooked

--- lua/main/init.lua
+++ lua/main/init.lua
@@ -1,5 +1,4 @@
 require("main.packer")
-require("main.flutter")
 
 --Theme
 require('lualine').setup({
@@ -42,8 +41,27 @@ lsp.setup_nvim_cmp({
   })
 })
 
+require("flutter-tools").setup {
+  decorations = {
+    statusline = {
+      app_version = false,
+      device = true,
+    },
+  },
+  widget_guides = {
+    enabled = true,
+  },
+  lsp = {
+    settings = {
+      showTodos = true,
+      completeFunctionCalls = true,
+      enableSnippets = true,
+    },
+  }
+}
+
 require("luasnip").filetype_extend("dart", { "flutter" })
-require("luasnip").filetype_extend("html", { "javascript", "css", "vue" })
+require("luasnip").filetype_extend("html", { "javascript", "css", "vue", "svelte" })
 
 lsp.setup()
 
@@ -65,15 +83,19 @@ vim.keymap.set('v', '<C-c>', '"+y')
 vim.keymap.set('v', '<C-x>', '"+x')
 vim.keymap.set('v', 'd', '"_d')
 vim.keymap.set('n', 'dd', '"_dd')
-vim.keymap.set('v','<Del>','"_x')
-vim.keymap.set('n','<Del>','"_x')
+vim.keymap.set('v', '<Del>', '"_x')
+vim.keymap.set('n', '<Del>', '"_x')
 vim.keymap.set('n', '<CR>', 'G<ESC>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gg=G', ':LspZeroFormat<CR>', { noremap = true , silent=true })
-vim.api.nvim_set_keymap('v', '<C-f>', '<ESC><cmd>lua vim.lsp.buf.format()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gg=G', ':LspZeroFormat<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-f>', '<ESC><cmd>lua vim.lsp.buf.format()<CR>', { noremap = true })
 vim.keymap.set('n', '<C-b><Up>', '<C-w><Up>')
 vim.keymap.set('n', '<C-b><Down>', '<C-w><Down>')
 vim.keymap.set('n', '<C-b><Left>', '<C-w><Left>')
 vim.keymap.set('n', '<C-b><Right>', '<C-w><Right>')
 vim.keymap.set('n', '<C-b>h', vim.cmd.sp)
 vim.keymap.set('n', '<C-b>v', vim.cmd.vsp)
----
+vim.keymap.set('i', '<C-s>', '<ESC>1z=')
+vim.api.nvim_set_keymap('n', '<C-S-Down>', ':m .+1<CR>==', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-Up>', ':m .-2<CR>==', { silent = true })
+vim.api.nvim_set_keymap('v', '<C-S-Down>', ":m '>+1<CR>gv=gv", { silent = true })
+vim.api.nvim_set_keymap('v', '<C-S-Up>', ":m '<-2<CR>gv=gv", { silent = true })
--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -1,28 +1,28 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {'nvim-lualine/lualine.nvim',requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
+  use 'wbthomason/packer.nvim' 
+  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lua'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
     }
   }
   use {
